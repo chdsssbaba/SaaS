40b60b6c859b88f91b82182f7593dceb7b915eb3
chdsssbaba <chdsssbaba5@gmail.com>
2025-12-17 13:00:00 +0530

    feat: implement role-based authorization middleware

---
+++ backend/src/middleware/authorize.js
@@ -0,0 +1,25 @@
+/**
+ * Authorization middleware - checks if user has required role
+ * @param {Array<string>} allowedRoles - Array of allowed roles
+ */
+function authorize(allowedRoles) {
+    return (req, res, next) => {
+        if (!req.user) {
+            return res.status(401).json({
+                success: false,
+                message: 'Authentication required'
+            });
+        }
+
+        if (!allowedRoles.includes(req.user.role)) {
+            return res.status(403).json({
+                success: false,
+                message: 'Forbidden. You do not have permission to access this resource.'
+            });
+        }
+
+        next();
+    };
+}
+
+module.exports = authorize;
